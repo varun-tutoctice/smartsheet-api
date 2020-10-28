8d63fb7d78e902df2380911fad53fc1b52235427
varun-tutoctice <40467753+varun-tutoctice@users.noreply.github.com>
2020-10-28 02:15:55 -0400

    App

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,22 @@
+export const environment = {
+  production: true,
+  // deploymentUrl: 'https://d2mjls5dggki99.cloudfront.net/home',
+  // smartSheetUrl: 'https://api.smartsheet.com/2.0',
+  // outlookUrl:'https://thingproxy.freeboard.io/fetch/https://outlook.office.com/api/v2.0',
+  // proxy:'https://cors-anywhere.herokuapp.com',
+  // tokenUrl:'https://login.microsoftonline.com/common/oauth2/v2.0/token'
+
+
+ // baseUrl:'https://d2mjls5dggki99.cloudfront.net',
+  //deploymentUrl: 'https://d2mjls5dggki99.cloudfront.net/home',
+
+
+  // baseUrl:'https://ec2-3-84-15-181.compute-1.amazonaws.com',
+  // deploymentUrl: 'https://ec2-3-84-15-181.compute-1.amazonaws.com/home',
+
+
+  baseUrl:'https://d3f2yqw0s1chue.cloudfront.net',
+  deploymentUrl: 'https://d3f2yqw0s1chue.cloudfront.net/home',
+  smartSheetUrl: 'https://6atloa7n7i.execute-api.us-east-1.amazonaws.com/dev',
+  tokenUrl:'https://6q5h3u6ky5.execute-api.us-east-1.amazonaws.com/dev'
+};
